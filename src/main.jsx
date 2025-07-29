4e4e89d51550f6323a8590b3b5279905633c051f
william-Dic <1700651360@qq.com>
2025-07-29 17:19:23 +0100

    Add simple React app test - isolate React rendering issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import SimpleApp from './SimpleApp.jsx'
 import GlobalStyle from './styles/global.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <SimpleApp />
   </React.StrictMode>,
 )
